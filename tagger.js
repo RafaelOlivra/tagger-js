a357e31c22e581c40258650a1de02985310651c2
Rafael Oliveira <contact@rafaeloliveiradesign.com>
2025-10-21 18:11:22 -0300

    Improve logging in Tagger module; add console message for successful remote data synchronization

--- tagger.js
+++ tagger.js
@@ -518,10 +518,10 @@ const tagger = {
                     if (responseData.data) {
                         // Response contains base64 encoded data
                         const decodedData = this._decodeRemoteData(responseData.data);
-                        console.log("[Tagger] Synced remote data.");
                         if (decodedData && this.isRemoteDataNewer(decodedData)) {
                             this.unlock(); // Unlock before applying data
                             this._applyRemoteData(decodedData);
+                            console.log("[Tagger] Synced remote data.");
                         } else {
                             console.log("[Tagger] Remote data is not newer or is invalid.");
                         }
